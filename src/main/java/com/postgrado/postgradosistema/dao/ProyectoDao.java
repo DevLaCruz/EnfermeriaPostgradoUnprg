a2fabac5e3fa6f9ee627883cf965c1d6212b74cc
DevLaCruz <97196517+DevLaCruz@users.noreply.github.com>
2024-02-25 13:31:48 -0500

    Modificar la funcion de buscar

--- src/main/java/com/postgrado/postgradosistema/dao/ProyectoDao.java
+++ src/main/java/com/postgrado/postgradosistema/dao/ProyectoDao.java
@@ -105,92 +105,122 @@ public class ProyectoDao {
     }
 
 
-    public List<Proyecto> listaProyectos() {
-        List<Proyecto> proyectos = new ArrayList<>();
-        String sql = "SELECT proyecto.id," +
-                " proyecto.titulo," +
-                " proyecto.asesora," +
-                " proyecto.jurado," +
-                " proyecto.res_designacion," +
-                " proyecto.res_ejecucion," +
-                " proyecto.res_cambioJurado," +
-                " proyecto.res_sustentacion," +
-                " proyecto.res_cambioTitulo," +
-                " proyecto.otros," +
-                " proyecto.especialidad_id," +
-                " especialidad.nombre AS nombre_especialidad," +
-                "proyecto.es_proyecto" +
-                " FROM proyecto" +
-                " INNER JOIN especialidad ON proyecto.especialidad_id = especialidad.id";
-        try {
-            cntn = cnxn.getConnection();
-            ps = cntn.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                Proyecto proyecto = new Proyecto();
-                proyecto.setId(rs.getInt("id"));
-                proyecto.setTitulo(rs.getString("titulo"));
-                proyecto.setAsesora(rs.getString("asesora"));
-                proyecto.setJurado(rs.getString("jurado"));
-                proyecto.setRes_designacion(rs.getString("res_designacion"));
-                proyecto.setRes_ejecucion(rs.getString("res_ejecucion"));
-                proyecto.setRes_cambioJurado(rs.getString("res_cambioJurado"));
-                proyecto.setRes_sustentacion(rs.getString("res_sustentacion"));
-                proyecto.setRes_cambioTitulo(rs.getString("res_cambioTitulo"));
-                proyecto.setOtros(rs.getString("otros"));
-                proyecto.setEspecialidad(new Especialidad(rs.getString("nombre_especialidad")));
-                proyecto.setEs_proyecto(rs.getString("es_proyecto"));
-                proyectos.add(proyecto);
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return proyectos;
-    }
+   public List<Proyecto> listaProyectos() {
+    List<Proyecto> proyectos = new ArrayList<>();
+    String sql = "SELECT proyecto.id," +
+            " proyecto.titulo," +
+            " proyecto.asesora," +
+            " proyecto.jurado," +
+            " proyecto.res_designacion," +
+            " proyecto.res_ejecucion," +
+            " proyecto.res_cambioJurado," +
+            " proyecto.res_sustentacion," +
+            " proyecto.res_cambioTitulo," +
+            " proyecto.otros," +
+            " proyecto.especialidad_id," +
+            " especialidad.nombre AS nombre_especialidad," +
+            " proyecto.es_proyecto," +
+            " ingresante1.nombre AS nombre_student1," +
+            " ingresante2.nombre AS nombre_student2," +
+            " ingresante3.nombre AS nombre_student3" +
+            " FROM proyecto" +
+            " INNER JOIN especialidad ON proyecto.especialidad_id = especialidad.id" +
+            " LEFT JOIN ingresante AS ingresante1 ON proyecto.id_student = ingresante1.id" +
+            " LEFT JOIN ingresante AS ingresante2 ON proyecto.id_student2 = ingresante2.id" +
+            " LEFT JOIN ingresante AS ingresante3 ON proyecto.id_student3 = ingresante3.id";
 
-    public List<Proyecto> buscarProyectoPorTitulo(String titulo) {
-        List<Proyecto> proyectos = new ArrayList<>();
-        String sql = "SELECT proyecto.id, " +
-                "proyecto.titulo, " +
-                "proyecto.asesora, " +
-                "proyecto.jurado, " +
-                "proyecto.res_designacion, " +
-                "proyecto.res_ejecucion, " +
-                "proyecto.res_cambioJurado, " +
-                "proyecto.res_sustentacion, " +
-                "proyecto.res_cambioTitulo, " +
-                "proyecto.otros, " +
-                "especialidad.nombre AS nombre_especialidad, " +
-                "proyecto.es_proyecto " +
-                "FROM proyecto " +
-                "INNER JOIN especialidad ON proyecto.especialidad_id = especialidad.id" +
-                " WHERE proyecto.titulo LIKE ?";
-        try {
-            cntn = cnxn.getConnection();
-            ps = cntn.prepareStatement(sql);
-            ps.setString(1, "%" + titulo + "%");
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                Proyecto proyecto = new Proyecto();
-                proyecto.setId(rs.getInt("id"));
-                proyecto.setTitulo(rs.getString("titulo"));
-                proyecto.setAsesora(rs.getString("asesora"));
-                proyecto.setJurado(rs.getString("jurado"));
-                proyecto.setRes_designacion(rs.getString("res_designacion"));
-                proyecto.setRes_ejecucion(rs.getString("res_ejecucion"));
-                proyecto.setRes_cambioJurado(rs.getString("res_cambioJurado"));
-                proyecto.setRes_sustentacion(rs.getString("res_sustentacion"));
-                proyecto.setRes_cambioTitulo(rs.getString("res_cambioTitulo"));
-                proyecto.setOtros(rs.getString("otros"));
-                proyecto.setEspecialidad(new Especialidad(rs.getString("nombre_especialidad")));
-                proyecto.setEs_proyecto(rs.getString("es_proyecto"));
-                proyectos.add(proyecto);
-            }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.toString());
+    try (Connection cntn = cnxn.getConnection();
+         PreparedStatement ps = cntn.prepareStatement(sql);
+         ResultSet rs = ps.executeQuery()) {
+
+        while (rs.next()) {
+            Proyecto proyecto = new Proyecto();
+            proyecto.setId(rs.getInt("id"));
+            proyecto.setTitulo(rs.getString("titulo"));
+            proyecto.setAsesora(rs.getString("asesora"));
+            proyecto.setJurado(rs.getString("jurado"));
+            proyecto.setRes_designacion(rs.getString("res_designacion"));
+            proyecto.setRes_ejecucion(rs.getString("res_ejecucion"));
+            proyecto.setRes_cambioJurado(rs.getString("res_cambioJurado"));
+            proyecto.setRes_sustentacion(rs.getString("res_sustentacion"));
+            proyecto.setRes_cambioTitulo(rs.getString("res_cambioTitulo"));
+            proyecto.setOtros(rs.getString("otros"));
+            proyecto.setEspecialidad(new Especialidad(rs.getString("nombre_especialidad")));
+            proyecto.setStudent1(new Ingresante(rs.getString("nombre_student1")));
+            proyecto.setStudent2(new Ingresante(rs.getString("nombre_student2")));
+            proyecto.setStudent3(new Ingresante(rs.getString("nombre_student3")));
+            proyecto.setEs_proyecto(rs.getString("es_proyecto"));
+
+            proyectos.add(proyecto);
         }
-        return proyectos;
+    } catch (SQLException e) {
+        System.out.println(e);
     }
+    return proyectos;
+}
+
+
+
+   public List<Proyecto> buscarProyectoPorTitulo(String titulo) {
+    List<Proyecto> proyectos = new ArrayList<>();
+    String sql = "SELECT proyecto.id, " +
+        "proyecto.titulo, " +
+        "proyecto.asesora, " +
+        "proyecto.jurado, " +
+        "proyecto.res_designacion, " +
+        "proyecto.res_ejecucion, " +
+        "proyecto.res_cambioJurado, " +
+        "proyecto.res_sustentacion, " +
+        "proyecto.res_cambioTitulo, " +
+        "proyecto.otros, " +
+        "especialidad.nombre AS nombre_especialidad, " +
+        "proyecto.es_proyecto, " +
+        "ingresante1.nombre AS nombre_student1," +
+        "ingresante2.nombre AS nombre_student2," +
+        "ingresante3.nombre AS nombre_student3 " +
+        "FROM proyecto " +
+        "INNER JOIN especialidad ON proyecto.especialidad_id = especialidad.id " +
+        "LEFT JOIN ingresante AS ingresante1 ON proyecto.id_student = ingresante1.id " +
+        "LEFT JOIN ingresante AS ingresante2 ON proyecto.id_student2 = ingresante2.id " +
+        "LEFT JOIN ingresante AS ingresante3 ON proyecto.id_student3 = ingresante3.id " +
+        
+        "WHERE proyecto.titulo LIKE ? OR ingresante1.nombre LIKE ? OR ingresante2.nombre LIKE ? OR ingresante3.nombre LIKE ?";
+
+    try {
+        cntn = cnxn.getConnection();
+        ps = cntn.prepareStatement(sql);
+        // Aquí establecemos el valor de búsqueda tanto para el título del proyecto como para los nombres de los estudiantes
+        String searchTerm = "%" + titulo + "%";
+        ps.setString(1, searchTerm);
+        ps.setString(2, searchTerm);
+        ps.setString(3, searchTerm);
+        ps.setString(4, searchTerm);
+        rs = ps.executeQuery();
+        while (rs.next()) {
+            Proyecto proyecto = new Proyecto();
+            proyecto.setId(rs.getInt("id"));
+            proyecto.setTitulo(rs.getString("titulo"));
+            proyecto.setAsesora(rs.getString("asesora"));
+            proyecto.setJurado(rs.getString("jurado"));
+            proyecto.setRes_designacion(rs.getString("res_designacion"));
+            proyecto.setRes_ejecucion(rs.getString("res_ejecucion"));
+            proyecto.setRes_cambioJurado(rs.getString("res_cambioJurado"));
+            proyecto.setRes_sustentacion(rs.getString("res_sustentacion"));
+            proyecto.setRes_cambioTitulo(rs.getString("res_cambioTitulo"));
+            proyecto.setStudent1(new Ingresante(rs.getString("nombre_student1")));
+            proyecto.setStudent2(new Ingresante(rs.getString("nombre_student2")));
+            proyecto.setStudent3(new Ingresante(rs.getString("nombre_student3")));
+            proyecto.setOtros(rs.getString("otros"));
+            proyecto.setEspecialidad(new Especialidad(rs.getString("nombre_especialidad")));
+            proyecto.setEs_proyecto(rs.getString("es_proyecto"));
+            proyectos.add(proyecto);
+        }
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, e.toString());
+        System.out.println(e);
+    }
+    return proyectos;
+}
 
     public List<Proyecto> buscarProyectoPorAño(String año) {
         List<Proyecto> proyectos = new ArrayList<>();
